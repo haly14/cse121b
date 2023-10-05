c71a98a1d87a948393ff9a4ba74fb5b46f25e5e7
haly14 <haly14@gmail.com>
2023-10-04 22:13:28 -0600

    partial work on w02task

---
+++ scripts/w02-task.js
@@ -0,0 +1,29 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = `Haley Atkinson`;
+let currentYear = `2023`;
+let profilePicture = `images/profile.png`;
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById(`name`);
+const foodElement = document.getElementById(`food`);
+const yearElement = document.querySelector(`#year`);
+const imageElement = document.getElementById,emt
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
