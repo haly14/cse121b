4d43a6734d7346bcb82475ca817a69e80402cab2
haly14 <haly14@gmail.com>
2023-10-21 13:56:11 -0600

    fixed w04 issues after clarification. fin last sec

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,50 +2,63 @@
 
 /* Profile Object  */
 let myProfile = {
-    name: "Haley Atkinson"
-    photo: "images/profile.png"
+    name: "Haley Atkinson",
+    photo: "images/profile.png",
     favoriteFoods: [
-        'Katsudon',
-        'Ice Cream',
-        'Potatoes'
-    ]
+        "Katsudon",
+        "Ice Cream",
+        "Potatoes"
+    ],
     hobbies: [
-        'Making Music',
-        'Hiking',
-        'Reading',
-        'Video Games'
-    ]
-    placesLived: [
-
-    ]
-}
+        "Making Music",
+        "Hiking",
+        "Reading",
+        "Video Games"
+    ],
+    placesLived: []
+};
 
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
-        place: 'Montana, USA',
-        length: '14 years'
+        place: "Montana, USA",
+        length: "14 years"
     }
 );
 
 /* DOM Manipulation - Output */
 
 /* Name */
-document.querySelector('#name').textContent = myProfile.name;
+document.querySelector("#name").textContent = myProfile.name;
 
 /* Photo with attributes */
-document.querySelector('#photo').textContent = myProfile.photo;
+document.getElementById("photo").src = myProfile.photo;
+document.getElementById("photo").alt = myProfile.name;
 
 /* Favorite Foods List*/
 myProfile.favoriteFoods.forEach(food => {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.textContent = food;
-    document.querySelector('#favorite-foods').appendChild(li);
-})
+    document.querySelector("#favorite-foods").appendChild(li);
+});
 
 /* Hobbies List */
-
+let hobbiesList = document.getElementById("hobbies");
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement("li");
+    li.textContent = hobby;
+    document.querySelector("#hobbies").appendChild(li);
+});
 
 /* Places Lived DataList */
+let placesLivedList = document.getElementById("places-lived");
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
 
+    document.querySelector("#places-lived").appendChild(dt);
+    document.querySelector("#places-lived").appendChild(dd);
+})
 
